995c8ba60c682ff0c5737566ac201457abe9909a
Naseer Dari <naseerdari01@gmail.com>
2019-03-11 16:59:01 -0500

    more work

--- main.go
+++ main.go
@@ -6,88 +6,92 @@ import (
 	"fmt"
 )
 
-const cellSize = 8
-
 type stack struct {
 	tos  int
 	data []byte
 }
 
 type VM struct {
-	s *stack
-	r *stack
+	cellSize int
+	s        *stack
+	r        *stack
 }
 
-func NewVM() *VM {
+func NewVM(bytesPerCell int) *VM {
 	return &VM{
+		bytesPerCell,
 		&stack{
 			0,
-			make([]byte, 124*cellSize),
+			make([]byte, 124*bytesPerCell),
 		},
 		&stack{
 			0,
-			make([]byte, 124*cellSize),
+			make([]byte, 124*bytesPerCell),
 		},
 	}
 }
 
-func (v *VM) push(val uint64) {
+func (v *VM) CellSize() int {
+	return v.cellSize
+}
+
+func (v *VM) PushCell(val uint64) {
 	binary.BigEndian.PutUint64(v.s.data[v.s.tos:], val)
-	v.s.tos += cellSize
+	v.s.tos += v.CellSize()
 }
 
-func (v *VM) push2(belowTop, top uint64) {
+func (v *VM) Push2Cell(belowTop, top uint64) {
 	binary.BigEndian.PutUint64(v.s.data[v.s.tos:], belowTop)
-	v.s.tos += cellSize
+	v.s.tos += v.CellSize()
 	binary.BigEndian.PutUint64(v.s.data[v.s.tos:], top)
-	v.s.tos += cellSize
+	v.s.tos += v.CellSize()
 }
 
-func (v *VM) pop() {
-	v.s.tos -= cellSize
+func (v *VM) PopCell() {
+	v.s.tos -= v.CellSize()
 }
 
-func (v *VM) rpush() {
-	v.s.tos -= cellSize
-	_ = copy(v.r.data[v.r.tos:v.r.tos+cellSize], v.s.data[v.s.tos:v.s.tos+cellSize])
-	v.r.tos += cellSize
+func (v *VM) RPushCell() {
+	v.s.tos -= v.CellSize()
+	_ = copy(v.r.data[v.r.tos:v.r.tos+v.CellSize()], v.s.data[v.s.tos:v.s.tos+v.CellSize()])
+	v.r.tos += v.CellSize()
 }
 
-func (v *VM) rpush2() {
-	_ = copy(v.r.data[v.r.tos:v.r.tos+2*cellSize], v.s.data[v.s.tos-2*cellSize:v.s.tos])
-	v.r.tos += 2 * cellSize
-	v.s.tos -= 2 * cellSize
+func (v *VM) RPush2Cell() {
+	_ = copy(v.r.data[v.r.tos:v.r.tos+2*v.CellSize()], v.s.data[v.s.tos-2*v.CellSize():v.s.tos])
+	v.r.tos += 2 * v.CellSize()
+	v.s.tos -= 2 * v.CellSize()
 }
 
-func (v *VM) rpop() {
-	v.r.tos -= cellSize
+func (v *VM) RPopCell() {
+	v.r.tos -= v.CellSize()
 }
 
-func (v *VM) fetch() {
-	ridx := int(binary.BigEndian.Uint64(v.s.data[v.s.tos-cellSize:]))
-	_ = copy(v.s.data[v.s.tos-cellSize:v.s.tos], v.r.data[ridx*cellSize:(ridx+1)*cellSize])
+func (v *VM) FetchCell() {
+	ridx := int(binary.BigEndian.Uint64(v.s.data[v.s.tos-v.CellSize():]))
+	_ = copy(v.s.data[v.s.tos-v.CellSize():v.s.tos], v.r.data[ridx*v.CellSize():(ridx+1)*v.CellSize()])
 }
 
-func (v *VM) fetch2() {
-	ridx := int(binary.BigEndian.Uint64(v.s.data[v.s.tos-cellSize:]))
-	v.s.tos -= cellSize
-	_ = copy(v.s.data[v.s.tos:(v.s.tos+2)*cellSize], v.r.data[ridx*cellSize:(ridx+2)*cellSize])
-	v.s.tos += 2 * cellSize
+func (v *VM) Fetch2Cell() {
+	ridx := int(binary.BigEndian.Uint64(v.s.data[v.s.tos-v.CellSize():]))
+	v.s.tos -= v.CellSize()
+	_ = copy(v.s.data[v.s.tos:(v.s.tos+2)*v.CellSize()], v.r.data[ridx*v.CellSize():(ridx+2)*v.CellSize()])
+	v.s.tos += 2 * v.CellSize()
 }
 
-func (v *VM) store() {
-	ridx := int(binary.BigEndian.Uint64(v.s.data[v.s.tos-cellSize:]))
-	_ = copy(v.r.data[ridx*cellSize:(ridx+1)*cellSize], v.s.data[v.s.tos-2*cellSize:v.s.tos-cellSize])
-	v.s.tos -= 2 * cellSize
+func (v *VM) StoreCell() {
+	ridx := int(binary.BigEndian.Uint64(v.s.data[v.s.tos-v.CellSize():]))
+	_ = copy(v.r.data[ridx*v.CellSize():(ridx+1)*v.CellSize()], v.s.data[v.s.tos-2*v.CellSize():v.s.tos-v.CellSize()])
+	v.s.tos -= 2 * v.CellSize()
 }
 
-func (v *VM) store2() {
-	ridx := int(binary.BigEndian.Uint64(v.s.data[v.s.tos-cellSize:]))
-	v.s.tos -= cellSize
-	_ = copy(v.r.data[ridx*cellSize:(ridx+2)*cellSize], v.s.data[v.s.tos-2*cellSize:v.s.tos])
-	v.s.tos -= 2 * cellSize
+func (v *VM) Store2Cell() {
+	ridx := int(binary.BigEndian.Uint64(v.s.data[v.s.tos-v.CellSize():]))
+	v.s.tos -= v.CellSize()
+	_ = copy(v.r.data[ridx*v.CellSize():(ridx+2)*v.CellSize()], v.s.data[v.s.tos-2*v.CellSize():v.s.tos])
+	v.s.tos -= 2 * v.CellSize()
 }
 
-func (v *VM) prn() string {
+func (v *VM) PrnDataStack() string {
 	return fmt.Sprintf("%s", hex.Dump(v.s.data[:v.s.tos]))
 }
--- main_test.go
+++ main_test.go
@@ -7,42 +7,44 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const cellSize = 8
+
 func TestPush(t *testing.T) {
-	v := NewVM()
-	v.push(10)
+	v := NewVM(cellSize)
+	v.PushCell(10)
 	assert.Equal(t, 10, int(binary.BigEndian.Uint64(v.s.data[v.s.tos-cellSize:])), "push should put element on tos")
 	assert.Equal(t, cellSize, v.s.tos, "tos should point at the starting byte of current element")
-	v.push(12)
+	v.PushCell(12)
 	assert.Equal(t, 12, int(binary.BigEndian.Uint64(v.s.data[v.s.tos-cellSize:])), "pushed element should replace tos")
 	assert.Equal(t, 10, int(binary.BigEndian.Uint64(v.s.data[v.s.tos-2*cellSize:])), "push should keep old data")
 	assert.Equal(t, 2*cellSize, v.s.tos, "tos should advance by 'cellSize' each push at the starting byte of current element")
 }
 
 func TestPush2(t *testing.T) {
-	v := NewVM()
-	v.push2(10, 12)
+	v := NewVM(cellSize)
+	v.Push2Cell(10, 12)
 	assert.Equal(t, 12, int(binary.BigEndian.Uint64(v.s.data[v.s.tos-cellSize:])), "push2 should put 2nd element on tos")
 	assert.Equal(t, 10, int(binary.BigEndian.Uint64(v.s.data[v.s.tos-2*cellSize:])), "push2 should put 1st element below tos")
 	assert.Equal(t, 2*cellSize, v.s.tos, "push2 should increment tos by 2 cellSize")
 }
 
 func TestRPush(t *testing.T) {
-	v := NewVM()
-	v.push(10)
-	v.rpush()
+	v := NewVM(cellSize)
+	v.PushCell(10)
+	v.RPushCell()
 	assert.Equal(t, 10, int(binary.BigEndian.Uint64(v.r.data[v.r.tos-cellSize:])), "rpush should put element on tos")
 	assert.Equal(t, cellSize, v.r.tos, "tos should point at the starting byte of current element")
 	assert.Equal(t, 0, v.s.tos, "element should be removed from S after rpush")
-	v.push(12)
-	v.rpush()
+	v.PushCell(12)
+	v.RPushCell()
 	assert.Equal(t, 12, int(binary.BigEndian.Uint64(v.r.data[v.r.tos-cellSize:])), "rpushed element should replace tos")
 	assert.Equal(t, 10, int(binary.BigEndian.Uint64(v.r.data[v.r.tos-2*cellSize:])), "rpush should keep old data")
 	assert.Equal(t, 2*cellSize, v.r.tos, "tos should advance by 'cellSize' each push at the starting byte of current element")
 }
 func TestRPush2(t *testing.T) {
-	v := NewVM()
-	v.push2(10, 12)
-	v.rpush2()
+	v := NewVM(cellSize)
+	v.Push2Cell(10, 12)
+	v.RPush2Cell()
 	assert.Equal(t, 12, int(binary.BigEndian.Uint64(v.r.data[v.r.tos-cellSize:])), "rpush2 should put 2nd element on tos")
 	assert.Equal(t, 10, int(binary.BigEndian.Uint64(v.r.data[v.r.tos-2*cellSize:])), "rpush2 should put 1st element below tos")
 	assert.Equal(t, 2*cellSize, v.r.tos, "rpush2 should increment tos twice")
@@ -50,85 +52,85 @@ func TestRPush2(t *testing.T) {
 }
 
 func TestPop(t *testing.T) {
-	v := NewVM()
-	v.push(10)
-	v.pop()
+	v := NewVM(cellSize)
+	v.PushCell(10)
+	v.PopCell()
 	assert.Equal(t, 0, v.s.tos, "tos should start at -cellSize")
-	v.push(1234)
-	v.push(12)
-	v.pop()
+	v.PushCell(1234)
+	v.PushCell(12)
+	v.PopCell()
 	assert.Equal(t, 1234, int(binary.BigEndian.Uint64(v.s.data[v.s.tos-cellSize:])), "pop should remove tos")
 	assert.Equal(t, cellSize, v.s.tos, "tos should descrement 'cellSize' each pop")
 }
 
 func TestRPop(t *testing.T) {
-	v := NewVM()
-	v.push(10)
-	v.rpush()
-	v.rpop()
+	v := NewVM(cellSize)
+	v.PushCell(10)
+	v.RPushCell()
+	v.RPopCell()
 	assert.Equal(t, 0, v.r.tos, "tos of empty stack should be 0")
-	v.push(1234)
-	v.rpush()
-	v.push(12)
-	v.rpush()
-	v.rpop()
+	v.PushCell(1234)
+	v.RPushCell()
+	v.PushCell(12)
+	v.RPushCell()
+	v.RPopCell()
 	assert.Equal(t, 1234, int(binary.BigEndian.Uint64(v.r.data[v.r.tos-cellSize:])), "rpop should remove tos")
 	assert.Equal(t, cellSize, v.r.tos, "tos should descrement 'cellSize' each rpop")
 }
 
 func TestStore(t *testing.T) {
-	v := NewVM()
+	v := NewVM(cellSize)
 	val, idx := uint64(1122), uint64(3)
-	v.push(val)
-	v.push(idx)
-	v.store()
+	v.PushCell(val)
+	v.PushCell(idx)
+	v.StoreCell()
 	assert.Equal(t, 1122, int(binary.BigEndian.Uint64(v.r.data[idx*cellSize:])), "store should put element at index")
 	assert.Equal(t, 0, v.r.tos, "store should keep tos unchanged")
 	assert.Equal(t, 0, v.s.tos, "store should remove two elements from S")
 }
 
 func TestFetch(t *testing.T) {
-	v := NewVM()
+	v := NewVM(cellSize)
 	val, idx := uint64(1122), uint64(3)
-	v.push(val)
-	v.push(idx)
-	v.store()
-	v.push(idx)
-	v.fetch()
+	v.PushCell(val)
+	v.PushCell(idx)
+	v.StoreCell()
+	v.PushCell(idx)
+	v.FetchCell()
 	assert.Equal(t, 1122, int(binary.BigEndian.Uint64(v.s.data[v.s.tos-cellSize:])), "fetch should put the correct data on tos")
 	assert.Equal(t, 1122, int(binary.BigEndian.Uint64(v.r.data[idx*cellSize:])), "fetch should keep the data in R")
 }
 
 func TestStore2(t *testing.T) {
-	v := NewVM()
+	v := NewVM(cellSize)
 	val1, val2, idx := uint64(112), uint64(311), uint64(5)
-	v.push(val1)
-	v.push(val2)
-	v.push(idx)
-	v.store2()
+	v.PushCell(val1)
+	v.PushCell(val2)
+	v.PushCell(idx)
+	v.Store2Cell()
 	assert.Equal(t, 0, v.s.tos, "tos should be at the start should be empty")
 	assert.Equal(t, 112, int(binary.BigEndian.Uint64(v.r.data[int(idx*cellSize):int((idx+1)*cellSize)])), "val1 should be at start of address")
 	assert.Equal(t, 311, int(binary.BigEndian.Uint64(v.r.data[int((idx+1)*cellSize):int((idx+2)*cellSize)])), "val2 should be one cellsize after address")
 }
 
 func TestFetch2(t *testing.T) {
-	v := NewVM()
+	v := NewVM(cellSize)
 	val1, val2, idx := uint64(112), uint64(311), uint64(5)
-	v.push(val1)
-	v.push(val2)
-	v.push(idx)
-	v.store2()
+	v.PushCell(val1)
+	v.PushCell(val2)
+	v.PushCell(idx)
+	v.Store2Cell()
 	assert.Equal(t, 0, v.s.tos, "tos should be at the start should be empty")
-	v.push(idx)
-	v.fetch2()
+	v.PushCell(idx)
+	v.Fetch2Cell()
 	assert.Equal(t, 311, int(binary.BigEndian.Uint64(v.s.data[v.s.tos-cellSize:])), "val1 should be below tos")
 	assert.Equal(t, 112, int(binary.BigEndian.Uint64(v.s.data[v.s.tos-2*cellSize:])), "val2 should be on tos")
 }
 
 func TestPrn(t *testing.T) {
-	v := NewVM()
-	v.push(10)
-	str := v.prn()
+	v := NewVM(cellSize)
+	v.PushCell(10)
+	str := v.PrnDataStack()
 	expected := "00000000  00 00 00 00 00 00 00 0a                           |........|\n"
 	assert.Equal(t, expected, str, "prn should print S correctly")
 }
